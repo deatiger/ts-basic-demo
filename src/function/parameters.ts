f4b65657c6a94c392096a022229fae1f1d1a4039
deatiger <deatiger1492@gmail.com>
2021-01-04 13:17:44 +0900

    Add sample code for Function Types

---
+++ src/function/parameters.ts
@@ -0,0 +1,26 @@
+// オプションパラメーターを持つ関数の例
+export const isUserSignedIn = (userId: string, username?: string): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 1: User is signed in! Username is', username)
+    return true
+  } else {
+    console.log('Function parameters sample 2: User is not signed in')
+    return false
+  }
+}
+
+// デフォルトパラメーターを持つ関数の例
+export const isUserSignedIn2 = (userId: string, username = 'NO NAME'): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 3: User is signed in! Username is', username)
+    return true
+  } else {
+    console.log('Function parameters sample 4: User is not signed in')
+    return false
+  }
+}
+
+// レストパラメーターを持つ関数の例
+export const sumProductsPrice = (...productsPrice: number[]): number => {
+  return productsPrice.reduce((prevTotal, productPrice) => prevTotal + productPrice, 0)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-// 01.環境構築 と 02.ESLint&Prettier 用のサンプルコード
+// 「01.環境構築」と「02.ESLint&Prettier」のサンプルコード
 // import World from './world'
 // const root: HTMLElement | null = document.getElementById('root')
 // const world = new World('Hello World!!!')
 // world.sayHello(root)
 
-// 03.基本の型定義
-import { anySample, notExist, primitiveSample, unknownSample } from './basic'
-anySample()
-notExist()
-primitiveSample()
-unknownSample()
+// 「03.基本の型定義」のサンプルコード
+// import { anySample, notExist, primitiveSample, unknownSample } from './basic'
+// anySample()
+// notExist()
+// primitiveSample()
+// unknownSample()
+
+// 「04.関数の型定義」のサンプルコード
+import { logMessage, logMessage5 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage5('Hello Type Signature!')
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('123')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(1000, 2000, 500, 3000, 250)
+console.log('Function parameters sample 5: Total price:', sum)
